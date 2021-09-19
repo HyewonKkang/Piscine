2e374dd23a0a001de6ed4ce98246368e6bca403c
HyewonKkang <68578916+HyewonKkang@users.noreply.github.com>
2021-09-19 23:43:05 +0900

    Update Rush01/main.c

--- C_Piscine_Rush_01/main.c
+++ C_Piscine_Rush_01/main.c
@@ -1,27 +1,27 @@
 #include "rush.h"
 
-int    main(int argc, char *argv[])
+int	main(int argc, char *argv[])
 {
-    int    **arr;
-    int    max_value;
+	int	**arr;
+	int	max_value;
 
-    if (argc != 2)
-    {
-        ft_error();
-        return (1);
-    }
-    max_value = count_digit(argv[1]) / 4;
-    if (!param_is_valid(argv[1], max_value))
-    {
-        ft_error();
-        return (1);
-    }
-    arr = create_table(argv[1], max_value);
-    if (!arr || resolve(arr, max_value, 1, 1))
-    {
-        ft_error();
-        return (1);
-    }
-    print_table(arr, max_value);
-    return (0);
+	if (argc != 2)
+	{
+		ft_error();
+		return (1);
+	}
+	max_value = count_digit(argv[1]) / 4;
+	if (!param_is_valid(argv[1], max_value))
+	{
+		ft_error();
+		return (1);
+	}
+	arr = create_table(argv[1], max_value);
+	if (!arr || resolve(arr, max_value, 1, 1))
+	{
+		ft_error();
+		return (1);
+	}
+	print_table(arr, max_value);
+	return (0);
 }
