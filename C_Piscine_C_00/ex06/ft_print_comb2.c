ee90480b2b97e629c7f6607419abcdb20792d418
HyewonKkang <gpffps369@gmail.com>
2021-09-20 21:08:10 +0900

    Update C00/ex06

--- C_Piscine_C_00/ex06/ft_print_comb2.c
+++ C_Piscine_C_00/ex06/ft_print_comb2.c
@@ -3,54 +3,43 @@
 /*                                                        :::      ::::::::   */
 /*   ft_print_comb2.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hykang <hykang@student.42.fr>              +#+  +:+       +#+        */
+/*   By: hykang <hykang@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/09/11 19:35:04 by hykang            #+#    #+#             */
-/*   Updated: 2021/09/20 17:55:56 by hykang           ###   ########.fr       */
+/*   Created: 2021/09/20 21:04:48 by hykang            #+#    #+#             */
+/*   Updated: 2021/09/20 21:06:06 by hykang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void	printNumbers(char a, char b, char c, char d)
+void	ft_putchar(char c)
 {
-	if (!(a == c && b == d) && (a <= c && b <= d))
-	{
-		write(1, &a, 1);
-		write(1, &b, 1);
-		write(1, " ", 1);
-		write(1, &c, 1);
-		write(1, &d, 1);
-		if (!(a == '9' && b == '8' && c == '9' && d == '9'))
-			write(1, ", ", 2);
-	}
+	write(1, &c, 1);
 }
 
 void	ft_print_comb2(void)
 {
-	char	a;
-	char	b;
-	char	c;
-	char	d;
+	int	x;
+	int	y;
 
-	a = '0';
-	while (a <= '9')
+	x = 0;
+	while (x <= 98)
 	{
-		b = '0';
-		while (b <= '9')
+		y = x + 1;
+		while (y <= 99)
 		{
-			c = '0';
-			while (c <= '9')
+			ft_putchar(x / 10 + '0');
+			ft_putchar(x % 10 + '0');
+			ft_putchar(' ');
+			ft_putchar(y / 10 + '0');
+			ft_putchar(y % 10 + '0');
+			if (x != 98 || y != 99)
 			{
-				d = '0';
-				while (d <= '9')
-				{
-					printNumbers(a, b, c, d++);
-				}
-				c++;
+				ft_putchar(',');
+				ft_putchar(' ');
 			}
-			b++;
+			y++;
 		}
-		a++;
+		x++;
 	}
 }
