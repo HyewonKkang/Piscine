89ada63bcf468fcf798aba962ae8b6e6af222b45
HyewonKkang <gpffps369@gmail.com>
2021-09-22 21:52:02 +0900

    Update C03/ex01

--- C_Piscine_C_03/ex01/ft_strncmp.c
+++ C_Piscine_C_03/ex01/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: hykang <hykang@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/20 20:22:37 by hykang            #+#    #+#             */
-/*   Updated: 2021/09/21 21:37:05 by hykang           ###   ########.fr       */
+/*   Updated: 2021/09/22 21:51:39 by hykang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@ int	ft_strncmp(char *s1, char *s2, unsigned int n)
 	unsigned int	i;
 
 	i = 0;
-	while (s1[i] && s2[i] && i < n)
+	while ((s1[i] || s2[i]) && i < n)
 	{
 		if (s1[i] > s2[i])
 			return (1);
@@ -23,7 +23,5 @@ int	ft_strncmp(char *s1, char *s2, unsigned int n)
 			return (-1);
 		i++;
 	}
-	if (i == n)
-		i--;
 	return (0);
 }
